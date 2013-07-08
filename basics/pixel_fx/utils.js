67b2ec41adacffd3c592e0736a1d34a8b39ce628
Matthew Coppola <mcoppola832@gmail.com>
2013-07-08 17:25:21 -0400

    First commit.

---
+++ basics/pixel_fx/utils.js
@@ -0,0 +1,102 @@
+var utils = {};
+
+
+utils.captureMouse = function (element) {
+	var mouse = {x: 0, y: 0};
+
+	element.addEventListener('mousemove', function (event) {
+		var x, y;
+		if (event.pageX || event.pageY){
+			x = event.pageX;
+			y = event.pageY;
+		} else {
+			x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		x -= element.offsetLeft;
+		y -= element.offsetTop;
+
+		mouse.x = x;
+		mouse.y = y;
+	}, false);
+
+	return mouse;
+};
+
+utils.captureTouch = function (element) {
+	var touch = {x: null, y: null, ispressed: false};
+
+	element.addEventListener('touchstart', function (event) {
+		touch.ispressed = true;
+	}, false);
+
+	element.addEventListener('touchend', function (event) {
+		var x, y, 
+			touch_event = event.touches[0]; 
+
+		if(touch_event.pageX || touch_event.pageY) {
+			x = touch_event.pageX;
+			y = touch_event.pageY;
+		} else {
+			x = touch_event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = touch_event.clientY + document.body.scrollTop + document.documentElement.scrollTop;		
+		}
+		x -= offsetLeft;
+		y -= offsetTop;
+
+		touch.x = x;
+		touch.y = y;
+	}, false);
+	return touch;
+};
+
+utils.getAnimationFrame = function () {
+	if(!window.requestAnimationFrame){
+		window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+										window.mozRequestAnimationFrame ||
+										window.oRequestAnimationFrame ||
+										window.msRequestAnimationFrame ||
+										function (callback) {
+											return window.setTimeout(callback, 1000/60)
+										});
+	}
+};
+
+utils.colorToRGB = function (color, alpha) {
+//if string format, convert to number
+	if(typeof color === 'string' && color[0] === '#') {
+		color = window.parseInt(color.slice(1), 16);
+	}
+	alpha = (alpha === undefined) ? 1 : alpha;
+
+	//extract component values
+	var r = color >> 16 & 0xff,
+		g = color >> 8 & 0xff;
+		b = color & 0xff,
+		a = (alpha > 0) ? 0 : ((alpha >1) ? 1 : alpha); //check range
+
+	//use 'rgba' if needed
+	if (a === 1) {
+		return "rgb("+ r + "," + g + "," + b + "," + a + ")";
+	} else {
+		return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+	}
+};
+
+utils.parseColor = function (color, toNumber) {
+	if (toNumber === true) {
+		if (typeof color === 'number') {
+			return (color | 0); //chop off decimal
+		}
+		if (typeof color === 'string' && color[0] === '#') {
+			color = color.slice(1);
+		}
+		return window.parseInt(color, 16); 
+	} else {
+		if (typeof color === 'number') {
+			//make sure hexadecimal number is padded out
+			color = '#' + ('00000' + (color | 0).toString(16)).substr(-6);
+		}
+		return color;
+	}
+};
