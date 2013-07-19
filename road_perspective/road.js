bd3f1535c20eb3221d3f115718e246f4c80d2a43
mcoppola832 <mcoppola832@gmail.com>
2013-07-19 17:26:00 -0400

    consolidated code, moved more things to road.js

--- road_perspective/road.js
+++ road_perspective/road.js
@@ -9,20 +9,14 @@ function Road (lineWidth, color) {
 	this.lineWidth = lineWidth;
 	this.areaLines = [];
 	this.lineCount = 20;
-	this.offsets = [];
-	corners_temp = [];
-	for(var i = 0; i < this.lineCount; i++) {
-/*		for(var k = 0; i < 4; i++) {
-			corners_temp[k] = Math.random();
-		}*/
-		this.offsets[i] = Math.random();
-	}
+	this.delay = 100;
+
 }
-Road.prototype.play = function (context, frame, delay, width, height) {
+Road.prototype.play = function (context, frame, width, height) {
 
 	for(var i = 0; i < this.lineCount; i++) {
-		this.areaLines[i] = new AreaLine(this.x - (i*delay), this.y - (i*delay));
-		this.areaLines[i].draw(context, frame - (i*delay), width, height, this.offsets[i], i);
+		this.areaLines[i] = new AreaLine(this.x - (i*this.delay), this.y - (i*this.delay));
+		this.areaLines[i].draw(context, frame - (i*this.delay), width, height, i);
 	}
 	
 }
@@ -33,7 +27,7 @@ function AreaLine (x, y) {
 	this.color = "#000000";
 	this.lineWidth = 2;
 }
-AreaLine.prototype.draw = function (context, frame, width, height, rand, i) {
+AreaLine.prototype.draw = function (context, frame, width, height, i) {
 	context.save();
 	//context.translate(this.x, this.y);	
 	context.lineWidth = this.lineWidth;
@@ -87,14 +81,26 @@ AreaLine.prototype.draw = function (context, frame, width, height, rand, i) {
 	context.restore();
 }
 
-function SnowFlake (x, y) {
+function SnowFlake (x, y, width, height) {
 	colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF",
 		"#FF00FF", "#C0C0C0"];
 	this.x = x;
 	this.y = y;
+	this.z = 1;
 	this.color = colors[Math.floor(Math.random()*colors.length)];
 	this.lineWidth = 3;
-	this.z = 1;
+
+	this.dx = Math.abs(x - width/2);
+	this.dy = Math.abs(y - height/2);
+	this.rate = ((this.dx+this.dy)/(width+height))*6.5;
+	if (this.dx > this.dy){
+		this.xScale = this.dx/this.dy;
+		this.yScale = 1;
+	}
+	else {
+		this.xScale = 1;
+		this.yScale = this.dy/this.dx;
+	}
 }
 SnowFlake.prototype.draw = function (context, frame, width, height) {
 	context.fillStyle = this.color;
@@ -116,5 +122,20 @@ SnowFlake.prototype.draw = function (context, frame, width, height) {
 
 	context.stroke();
 	context.restore();
+
+	rate = Math.max(this.z, 0.3)*this.rate;
+
+	if(this.x > width/2) {
+		this.x -= 1/this.yScale*rate;
+	}
+	else if (this.x < width/2) {
+		this.x += 1/this.yScale*rate;
+	}
+	if(this.y > height/2) {
+		this.y -= 1/this.xScale*rate;
+	}
+	else if (this.y < height/2) {
+		this.y += 1/this.xScale*rate;
+	}
 
 }
