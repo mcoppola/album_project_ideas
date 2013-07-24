ad0e5731b90b1075a33fb29ac35622b9deb0973f
mcoppola832 <mcoppola832@gmail.com>
2013-07-23 20:27:35 -0400

    Trails branch, first commit

--- road_perspective/road.js
+++ road_perspective/road.js
@@ -24,6 +24,7 @@ Road.prototype.play = function (context, frame, width, height) {
 function AreaLine (x, y) {
 	this.x = x;
 	this.y = y;
+	this.z = 1;
 	this.color = "#000000";
 	this.lineWidth = 2;
 }
@@ -82,17 +83,21 @@ AreaLine.prototype.draw = function (context, frame, width, height, i) {
 }
 
 function SnowFlake (x, y, width, height) {
-	colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF",
-		"#FF00FF", "#C0C0C0"];
+	//colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF",
+	//	"#FF00FF", "#C0C0C0"];
 	this.x = x;
 	this.y = y;
 	this.z = 1;
-	this.color = colors[Math.floor(Math.random()*colors.length)];
+	this.delay = 100;
+	//this.color = colors[Math.floor(Math.random()*colors.length)];
 	this.lineWidth = 3;
 
+
 	this.dx = Math.abs(x - width/2);
+	this.dxu = x - width/2;
 	this.dy = Math.abs(y - height/2);
-	this.rate = ((this.dx+this.dy)/(width+height))*6.5;
+	this.dyu = y - height/2;
+	this.rate = (Math.sqrt(Math.pow(this.dx, 2)+Math.pow(this.dy, 2))/Math.sqrt(Math.pow(width/2, 2)+Math.pow(height/2, 2)))*3;
 	if (this.dx > this.dy){
 		this.xScale = this.dx/this.dy;
 		this.yScale = 1;
@@ -101,9 +106,26 @@ function SnowFlake (x, y, width, height) {
 		this.xScale = 1;
 		this.yScale = this.dy/this.dx;
 	}
+
+	this.trails = [];
+	for (var i = 1; i < Math.random()*10; i++) {
+		if(this.dxu < 0 && this.dyu < 0){
+			this.trails.push(new SnowFlakeTrails(this.x - (i*this.delay*(this.dx/(width/2))), this.y - (i*(this.delay*(this.dy/(height/2)))), width, this.rate));
+		}
+		else if(this.dxu > 0 && this.dyu < 0){
+			this.trails.push(new SnowFlakeTrails(this.x + (i*this.delay*(this.dx/(width/2))), this.y - (i*(this.delay*(this.dy/(height/2)))), width, this.rate));
+		}
+		else if(this.dxu < 0 && this.dyu > 0){
+			this.trails.push(new SnowFlakeTrails(this.x - (i*this.delay*(this.dx/(width/2))), this.y + (i*(this.delay*(this.dy/(height/2)))), width, this.rate));
+		}
+		else if(this.dxu > 0 && this.dyu > 0){
+			this.trails.push(new SnowFlakeTrails(this.x + (i*this.delay*(this.dx/(width/2))), this.y + (i*(this.delay*(this.dy/(height/2)))), width, this.rate));
+		}
+	}
+
 }
 SnowFlake.prototype.draw = function (context, frame, width, height) {
-	context.fillStyle = this.color;
+	
 	context.save();
 /*	context.translate(this.x, this.y);
 	context.rotate(Math.PI*z);*/
@@ -120,7 +142,84 @@ SnowFlake.prototype.draw = function (context, frame, width, height) {
 	context.moveTo(this.x, this.y);
 	context.lineTo(this.x - 8*this.z, this.y - 8*this.z);
 
+
+
+	//context.strokeStyle = this.color;
+
 	context.stroke();
+
+	context.restore();
+
+	for(var i = 0; i <this.trails.length; i++) {
+		this.trails[i].draw(context, frame, width, height);
+	}
+
+
+	rate = Math.max(this.z, 0.3)*this.rate;
+
+	if(this.x > width/2) {
+		this.x -= 1/this.yScale*rate;
+	}
+	else if (this.x < width/2) {
+		this.x += 1/this.yScale*rate;
+	}
+	if(this.y > height/2) {
+		this.y -= 1/this.xScale*rate;
+	}
+	else if (this.y < height/2) {
+		this.y += 1/this.xScale*rate;
+	}
+
+}
+
+function SnowFlakeTrails (x, y, width, height, rate) {
+	colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF",
+		"#FF00FF", "#C0C0C0"];
+	this.x = x - 50;
+	this.y = y - 50;
+	this.z = 1;
+	this.color = colors[Math.floor(Math.random()*colors.length)];
+	this.lineWidth = 2;
+
+
+	this.dx = Math.abs(x - width/2);
+	this.dy = Math.abs(y - height/2);
+	this.rate = rate;
+	if (this.dx > this.dy){
+		this.xScale = this.dx/this.dy;
+		this.yScale = 1;
+	}
+	else {
+		this.xScale = 1;
+		this.yScale = this.dy/this.dx;
+	}
+
+}
+
+SnowFlakeTrails.prototype.draw = function (context, frame, width, height) {
+	
+	context.save();
+/*	context.translate(this.x, this.y);
+	context.rotate(Math.PI*z);*/
+	context.lineWidth = this.lineWidth;
+	context.beginPath();
+	
+
+	context.moveTo(this.x, this.y);
+	context.lineTo(this.x + 4*this.z, this.y + 4*this.z);
+	context.moveTo(this.x, this.y);
+	context.lineTo(this.x - 4*this.z, this.y + 4*this.z);
+	context.moveTo(this.x, this.y);
+	context.lineTo(this.x + 4*this.z, this.y - 4*this.z);
+	context.moveTo(this.x, this.y);
+	context.lineTo(this.x - 4*this.z, this.y - 4*this.z);
+
+
+
+	context.strokeStyle = this.color;
+
+	context.stroke();
+
 	context.restore();
 
 	rate = Math.max(this.z, 0.3)*this.rate;
